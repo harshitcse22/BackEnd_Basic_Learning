04d9cfde35a8bba0ea8f3d4204e2ddb3941cf391
Harshit <hktiwari980@gmail.com>
2025-09-03 12:37:01 +0530

    more

--- server.js
+++ server.js
@@ -9,9 +9,15 @@
 const express = require('express');
 const app = express();
 
+//used to parse req.body (request body) in express -> PUT or POST
+const bodyParser = require('body-parser');
+
+//specifically parse JSON data & add it to the request.body object
+app.use(bodyParser.json());
+
 // activate the server on 3000 port
-app.listen(3000,() =>{
-    console.log("Server Started at port no. 3000")
+app.listen(8000,() =>{
+    console.log("Server Started at port no. 8000")
 });
 
 
@@ -26,3 +32,11 @@ app.post('/api/cars', (req,res) =>{
      console.log(brand);
      res.send("car submitted successfully");
 })
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017//myDatabase',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+.then(() => {console.log("Connection Successful")})
+.catch( (error) => {console.log("Recievd an error")});
